21f6189704770f8765110617ab19ac21d7e91588
Rosny Fletez Jr <rfletezjr@gmail.com>
2022-11-11 08:54:35 -0700

    Second commit
    Got started with the App component first.

--- WeConnectApp/src/app/app.component.ts
+++ WeConnectApp/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from './login/login.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WeConnectApp';
+  logout = false;
+  uProfile = false;
+  cProfile = false;
+
+  constructor(private router: Router, private loginService: LoginService) {
+    router.events.subscribe((val) => {
+      this.logout = false;
+      this.uProfile = false;
+      this.cProfile = false;
+
+      if(router.url === '/user/home' || router.url === '/user/profile' || router.url === '/user/appointments') {
+        this.logout = true;
+        this.uProfile = true;
+        this.cProfile = false;
+      }
+      else if(router.url === '/coach/home' || router.url === '/coach/profile') {
+        this.logout = true;
+        this.uProfile = false;
+        this.cProfile = true;
+      }
+      else {
+        this.logout = false;
+        this.uProfile = false;
+        this.cProfile = false;
+      }
+    });
+  }
+
+  logoutFn() {
+    this.router.navigate(['/home']);
+  }
+  
 }
