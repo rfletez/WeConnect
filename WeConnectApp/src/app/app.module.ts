183f90574ebb63e45c48834e90fd9bb11fcabe53
Rosny Fletez Jr <rfletezjr@gmail.com>
2022-11-11 12:36:50 -0700

    Modified Routing in App component.

--- WeConnectApp/src/app/app.module.ts
+++ WeConnectApp/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +9,11 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { CoachComponent } from './coach/coach.component';
+import { UserComponent } from './user/user.component';
+import { UserModule } from './user/user/user.module';
+import { CoachModule } from './coach/coach/coach.module';
+import { LoginService } from './login/login.service';
+import { SignupService } from './signup/signup.service';
 
 @NgModule({
   declarations: [
@@ -14,13 +21,22 @@ import { CoachComponent } from './coach/coach.component';
     HomeComponent,
     LoginComponent,
     SignupComponent,
-    CoachComponent
+    CoachComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    UserModule,
+    CoachModule
   ],
-  providers: [],
+  providers: [
+    LoginService,
+    SignupService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
