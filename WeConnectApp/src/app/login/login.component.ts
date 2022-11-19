7edd3d2f2f42b83fba5b76bb3795e98ef26d7b16
Rosny Fletez Jr <rfletezjr@gmail.com>
2022-11-19 14:17:58 -0700

    Modified Login TS.
    I will need to check if this is how to do it. Or use the LoginService or the Guard.

--- WeConnectApp/src/app/login/login.component.ts
+++ WeConnectApp/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private loginService: LoginService) { }
 
   ngOnInit(): void {
   }
 
+  joinAsCoach() {
+    this.router.navigate(['/signup/coaches']); //route path: '/signup/coaches', component: SignupComponent
+  }
+  loginAsCoach() {
+    this.router.navigate(['/login/coaches']); //route path: '/login/coaches', component: LoginComponent
+  }
+
+  joinAsUser() {
+    this.router.navigate(['/signup/users']); //route path: '/signup/users', component: SignupComponent
+  }
+  loginAsUser() {
+    this.router.navigate(['/login/users']); //route path: '/login/users', component: LoginComponent
+  }
 }
